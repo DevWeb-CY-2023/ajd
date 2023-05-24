4c7cb25b2f8da5100a9a19bc000f9cf2dc4c3756
ValentinLaTortue <106513308+ValentinLaTortue@users.noreply.github.com>
2023-05-24 18:18:59 +0200

    modif creationjeune

--- creationjeune.php
+++ creationjeune.php
@@ -1,11 +1,8 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Résultats du formulaire</title>
 </head>
 <body>
-	<h1>Résultats du formulaire</h1>
-
 	<?php
 		function utilisateurExiste($utilisateur){  //retourne true si l'email du nouveau utilisateur existe déja, false sinon
 			$dataJeune = fopen("dataJeune.csv","r");
@@ -42,28 +39,14 @@
 			$choix8 = $_POST["choix8"];
 			$choix9 = $_POST["choix9"];
 			$choix10 = $_POST["choix10"];
-			
-			
-			// Affiche les valeurs des champs de formulaire
-			echo "<p>Nom: " . $nom . "</p>";
-			echo "<p>Prénom: " . $prenom . "</p>";
 
 			$dataJeune = fopen("dataJeune.csv","a");
 
 			if(!utilisateurExiste($email)){
-				echo "réussite de la mission";
 				fwrite($dataJeune, $nom.";".$prenom.";".$mdp.";".$birthday.";".$email.";".$reseauSocial.";".$centreInteret.";".$engagement.";".$duree.";".$choix1.";".$choix2.";".$choix3.";".$choix4.";".$choix5.";".$choix6.";".$choix7.";".$choix8.";".$choix9.";".$choix10.
 ";\n");}
-			else{
-				echo "échec de la mission";
-			}
 			fclose($dataJeune);
-			echo "<br>"."fini";
-
-
-
-
-
+			header("Location: connexion.html");
 
 		} else {
 			// Si le formulaire n'a pas été soumis, affiche un message d'erreur
