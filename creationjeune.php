efd07a3d66c760e2c92c898867a3908a37ae59cb
ValentinLaTortue <106513308+ValentinLaTortue@users.noreply.github.com>
2023-05-24 18:07:35 +0200

    modif page4, ajout creationjeune.php et dataJeune.csv

---
+++ creationjeune.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Résultats du formulaire</title>
+</head>
+<body>
+	<h1>Résultats du formulaire</h1>
+
+	<?php
+		function utilisateurExiste($utilisateur){  //retourne true si l'email du nouveau utilisateur existe déja, false sinon
+			$dataJeune = fopen("dataJeune.csv","r");
+			
+			while(!feof($dataJeune)){
+				$line=fgets($dataJeune);
+				$mots=explode(";", $line);
+				
+				if($mots[4] == $utilisateur){ return true;}//compare l'email du nouveau compte à chacun des emails déja utilisé
+			}
+			return false;
+		}
+		// Vérifie si le formulaire a été soumis
+		if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+			// Récupère les valeurs des champs de formulaire
+			$nom = $_POST["nom"];
+			$prenom = $_POST["prenom"];
+			$mdp = $_POST["mdp"];
+			$birthday = $_POST["birthday"];
+			$email = $_POST["email"];
+			$reseauSocial = $_POST["reseauSocial"];
+			$centreInteret = $_POST["centreInteret"];
+			$engagement = $_POST["engagement"];
+			$duree = $_POST["duree"];
+
+			$choix1 = $_POST["choix1"];
+			$choix2 = $_POST["choix2"];
+			$choix3 = $_POST["choix3"];
+			$choix4 = $_POST["choix4"];
+			$choix5 = $_POST["choix5"];
+			$choix6 = $_POST["choix6"];
+			$choix7 = $_POST["choix7"];
+			$choix8 = $_POST["choix8"];
+			$choix9 = $_POST["choix9"];
+			$choix10 = $_POST["choix10"];
+			
+			
+			// Affiche les valeurs des champs de formulaire
+			echo "<p>Nom: " . $nom . "</p>";
+			echo "<p>Prénom: " . $prenom . "</p>";
+
+			$dataJeune = fopen("dataJeune.csv","a");
+
+			if(!utilisateurExiste($email)){
+				echo "réussite de la mission";
+				fwrite($dataJeune, $nom.";".$prenom.";".$mdp.";".$birthday.";".$email.";".$reseauSocial.";".$centreInteret.";".$engagement.";".$duree.";".$choix1.";".$choix2.";".$choix3.";".$choix4.";".$choix5.";".$choix6.";".$choix7.";".$choix8.";".$choix9.";".$choix10.
+";\n");}
+			else{
+				echo "échec de la mission";
+			}
+			fclose($dataJeune);
+			echo "<br>"."fini";
+
+
+
+
+
+
+		} else {
+			// Si le formulaire n'a pas été soumis, affiche un message d'erreur
+			echo "<p>Le formulaire n'a pas été soumis.</p>";
+		}
+	?>
+</body>
+</html>
